54a4d76eb4a992dd881c9b48f10a02641a73f087
crusaderky <crusaderky@gmail.com>
2024-02-13 12:33:58 +0000

    Fail on all warnings (#6)

--- ci/mongo_smoke_test.py
+++ ci/mongo_smoke_test.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    client = MongoClient(connectTimeoutMS=5000)
+    client: MongoClient = MongoClient(connectTimeoutMS=5000)
     db_list = client.list_database_names()
     print(db_list)
