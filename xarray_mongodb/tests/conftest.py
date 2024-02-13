072ab1c0ab8730f214b25142124577f691b4c45a
crusaderky <crusaderky@gmail.com>
2024-02-13 12:21:40 +0000

    Fail on all warnings

--- xarray_mongodb/tests/conftest.py
+++ xarray_mongodb/tests/conftest.py
@@ -1,6 +1,5 @@
 """pytest fixtures
 """
-import asyncio
 
 import pymongo
 import pytest
@@ -22,16 +21,7 @@ def sync_xdb(sync_db):
 
 
 @pytest.fixture(scope="function")
-def event_loop():
-    """pytest-asyncio by default creates a new event loop for every new coroutine. This
-    happens *after* the fixtures are applied, which causes the AsyncIOMotorClient to be
-    attached to the wrong event loop
-    """
-    yield asyncio.get_event_loop()
-
-
-@pytest.fixture(scope="function")
-async def async_db(event_loop):
+async def async_db():
     import motor.motor_asyncio
 
     client = motor.motor_asyncio.AsyncIOMotorClient()
