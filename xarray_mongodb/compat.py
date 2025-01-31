ec4e524e8c290fd268577a9c959961d3b789177b
Guido Imperiale <crusaderky@gmail.com>
2025-01-31 14:10:39 +0000

    sparse cleanup (#13)

--- xarray_mongodb/compat.py
+++ xarray_mongodb/compat.py
@@ -1,4 +1,4 @@
-"""Import pint and sparse objects or dummies
+"""Import pint objects or dummies
 
 Please read :doc:`nep18`.
 """
@@ -8,7 +8,7 @@ from __future__ import annotations
 from collections.abc import Callable
 from typing import Any
 
-__all__ = ("COO", "Quantity", "Unit", "UnitRegistry", "has_motor")
+__all__ = ("Quantity", "Unit", "UnitRegistry", "has_motor")
 
 
 try:
@@ -17,19 +17,6 @@ try:
     has_motor = motor.version_tuple >= (2, 3)
 except ImportError:
     has_motor = False
-
-
-# sparse imports or dummies
-try:
-    from sparse import COO
-
-except ImportError:
-    # Dummy objects that are not equal to anything else and for which isinstance()
-    # always returns False. Can't just use `COO = object()` because things like
-    # Optional[COO] don't like it.
-
-    class COO:  # type: ignore[no-redef]
-        pass
 
 
 # pint imports or dummies
